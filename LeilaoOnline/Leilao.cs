d6d2f9b835bb612cf6f81ec2d17d9a86d09c1af1
Victor <victor.ananias8@gmail.com>
2020-03-19 10:14:18 -0300

    setup inicial

---
+++ LeilaoOnline/Leilao.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeilaoOnline
+{
+    public class Leilao
+    {
+        private IList<Lance> _lances;
+        public IEnumerable<Lance> Lances => _lances;
+        public string Peca { get; }
+
+        public Leilao(string peca)
+        {
+            Peca = peca;
+            _lances = new List<Lance>();
+        }
+
+        public void RecebeLance(Interessada cliente, double valor)
+        {
+            _lances.Add(new Lance(cliente, valor));
+        }
+
+        public void IniciaPregao()
+        {
+
+        }
+
+        public void TerminaPregao()
+        {
+
+        }
+    }
+}
